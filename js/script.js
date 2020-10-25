67cc8623775fc512b5183ecf7b8e4add99f0cc9f
Caterina <danizio@outlook.it>
2020-10-25 20:38:47 +0100

    Ordine alfabetico e posizione

--- js/script.js
+++ js/script.js
@@ -13,18 +13,40 @@ function () {
 var cognomeUser = document.getElementById('cognomeUser').value;
 console.log("Il cognome inserito è ",cognomeUser)
 
+// Se l'iniziale inserita non è maiuscola
+cognomeUser = cognomeUser.charAt(0).toUpperCase() + cognomeUser.slice(1);
 
 
-// Inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+
+// Inserirlo nell'array originale
 cognomi.push(cognomeUser);
+console.log("L'array aggiornato è " , cognomi);
 
-console.log("L'array aggiornato è " , cognomi)
 
 // Stampare la lista ordinata alfabeticamente
+cognomi.sort();
+console.log("La lista in ordine alfabetico è ")
+
+var listaStampata = "";
+    var i = 0;
+    while (i < cognomi.length) {
+      listaStampata = listaStampata + "<li>" + cognomi[i] + "</li>";
+      i++
+    }
+
+  document.getElementById('stampa').innerHTML = 'LISTA COGNOMI <br>' + listaStampata;
+
 
 // Scrivere anche la posizione “umana” della lista in cui il nuovo utente si trova
-
-
+// scrivo numero posizione
+    i = 0
+    while (i < cognomi.length) {
+      if (cognomi[i] === cognomeUser) {
+        console.log("sei il numero:", i+1 );
+        var posizione = i+1
+      }
+      i++
+    }
 });
 
 // Funzionalità tasto annulla
